a39714259aa47595792cb371a99a28531575bf2f
Johanna <godoy.johanna@gmx.ch>
2024-03-17 11:07:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "dbt94cfe51acce06f03989b19ad32fo0";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
